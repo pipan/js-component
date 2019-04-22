c5076bc1101d1825a6be34f869eb5b689de3d8b8
Peter Gasparik <gasparik.pe@gmail.com>
2019-04-22 13:36:15 +0200

    remove initialize from component interface

--- src/Component.ts
+++ src/Component.ts
@@ -2,7 +2,6 @@ import { Emitter } from '@wildebeest/common';
 
 export interface Component
 {
-    initialize(element: HTMLElement): void;
     getElement(): HTMLElement;
     getEmitter(): Emitter;
 }
